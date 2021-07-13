2bcdfd76abbf2afd0970735be7f264199c66d75f
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-13 15:11:21 +0200

    add mines random

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /**
  * ! Consegna
- * Il computer deve generare 16 numeri casuali tra 1 e 100, queste saranno le nostre bombe.
+ * Il computer deve generare 16 numeri casuali tra 1 e 100, queste saranno le nostre bombe. //* OK
  * I numeri delle bombe non possono essere duplicati (i 16 numeri devono essere tutti diversi)
  * Il giocatore, deve cercare di non prendere le bombe. Gli chiederemo 100 - 16 volte di scegliere un numero, uno alla volta, sempre compreso tra 1 e 100.
  * L'utente non può inserire 2 volte lo stesso numero
@@ -16,4 +16,58 @@
  * TODO: Bonus #3: Stampa in pagina in file separato ma solo dopo che l'esercizio base sia corretto 
  */
 
-console.log("here!");
+// ? 1: dichiarazione variabili 
+
+const mines =[0];
+
+//in un futuro posso gestire maxMines --cambiando anche scope della variabile
+const maxMines=16; 
+let maxChoice= 100 - maxMines; 
+const min=1;
+const max=100; //// : function random 100 incluso
+
+fillArrayRandom(mines,maxMines);
+console.log("********* Queste sono le mine: *********");
+console.table(mines);
+
+
+
+
+
+
+
+
+
+
+
+//TODO:funzione di riempiemnto scelte utente con maxChioce elementi da riempire + controllo duplicati
+//TODO:validazione input utente
+
+//// funzione fillArrayRandom: deve riempire l'array passato di numeri casuali ,max elementi da riempire
+/** riempe un array di numeri casuali 
+ * 
+ * @param {number} arr  di numeri casuali
+ * @param {number} maxElement  elementi massimi in arr
+ */
+ function fillArrayRandom(arr,maxElement){
+     let current=0;
+    for(let i=0;i<maxElement;i++){
+        current=getRandomNumber(max,min);
+        //? controlla che il valore non sia già presente in arr in caso contrario estrae un altro numero casuale
+        if (arr.includes(current)){
+            current=getRandomNumber(max,min);
+        }
+        arr[i]=current;
+    }
+ }
+
+/** ritorna un numero casuale estremi inclusi
+ * 
+ * @param {number} maxRange  massimo incluso
+ * @param {number} minRange  minimo incluso
+ * @returns  un numero casuale tra min e max
+ */
+function getRandomNumber(maxRange,minRange){
+    maxRange++;
+    return Math.floor(Math.random()*(maxRange - minRange))+minRange;
+}
